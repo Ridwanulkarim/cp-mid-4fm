7d298ae164d7c4c904651d4d2793fdf4c5efff1a
Ridwanulkarim <ridwanulk08@gmail.com>
2026-05-09 23:02:00 +0600

    Add files via upload

---
+++ binarysearchonans.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter number: ";
+    cin >> n;
+    int low = 1;
+    int high = n;
+    int ans = 0;
+    while(low <= high) {
+        int mid = (low + high) / 2;
+        if(mid * mid <= n) {
+            ans = mid;
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    cout << "Square Root = " << ans;
+    return 0;
+}
